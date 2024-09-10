2c36a1f210658af33960f0666c5098877d9119e0
Johleo77 <johanleonard77@gmail.com>
2024-09-10 08:55:34 +0200

    exo fait

--- 15-Structures_de_controle/15_2-when/P15_2_a-Harry_Potter/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_2-when/P15_2_a-Harry_Potter/src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
 fun main() {
-    // write your code here
+    var a = "gryffindor"
+    when(a){
+        "gryffindor" -> println("bravery")
+        "hufflepuff" -> println("loyalty")
+        "slytherin" -> println("cunning")
+        "ravenclaw" -> println("intellect")
+        else -> println("not a valid house.")
+    }
 }

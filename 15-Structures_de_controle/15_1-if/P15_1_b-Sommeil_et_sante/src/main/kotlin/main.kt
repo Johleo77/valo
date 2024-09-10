81aabd1f263808e2e2e6af481e4bd3be94726bda
Johleo77 <johanleonard77@gmail.com>
2024-09-10 08:39:03 +0200

    exo fait

--- 15-Structures_de_controle/15_1-if/P15_1_b-Sommeil_et_sante/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_b-Sommeil_et_sante/src/main/kotlin/main.kt
@@ -1,3 +1,3 @@
-fun main() {
-    // write your code here
+ fun main() {
+
 }
--- 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_1-if/P15_1_c-nombre_diviseurs/src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    // put your code here
+    var number = 6
+    if (number % 2 == 0){
+          println("divisible par 2")}
+    if (number % 3 == 0){
+        println("divisible par 3")}
+    if (number % 6 == 0){
+        println("divisible par 6")}
 }

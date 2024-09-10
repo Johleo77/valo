f9f6963f2a8d213df4c916cfcc25768951a4e9f5
Johleo77 <johanleonard77@gmail.com>
2024-09-10 09:39:31 +0200

    exo fait

--- 15-Structures_de_controle/15_5-Boucle_for/P15_5_b-Minimal_de_N_nombres/src/main/kotlin/main.kt
+++ 15-Structures_de_controle/15_5-Boucle_for/P15_5_b-Minimal_de_N_nombres/src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    // write your code here
+    var n = mutableListOf(5, 5, 1, 4, 2, 3)
+    var i = 0
+    var res = n[0]
+    for (i in n) {
+        if (n[i] < res) {
+            res = n[i]
+        }
+    }
+    println(res)
 }
+
+
+

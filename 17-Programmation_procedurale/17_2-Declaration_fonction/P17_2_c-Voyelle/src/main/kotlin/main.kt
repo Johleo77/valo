3cb79612b26715e2902a176c410432457035028d
Johleo77 <johanleonard77@gmail.com>
2024-09-17 09:14:00 +0200

    exo fait

--- 17-Programmation_procedurale/17_2-Declaration_fonction/P17_2_c-Voyelle/src/main/kotlin/main.kt
+++ 17-Programmation_procedurale/17_2-Declaration_fonction/P17_2_c-Voyelle/src/main/kotlin/main.kt
@@ -1,4 +1,8 @@
-// write your function here
+fun isVowel(lettre : Char){
+    val voyelle = listOf('a','e','i','o','u','y')
+    val minuscule = lettre.toLowerCase()
+    println(voyelle.contains(minuscule))
+}// write your function here
 
 fun main() {
     val letter = readLine()!!.first()
